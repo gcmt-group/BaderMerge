0c07ceca7cedddf35f17210de91b4bfecae3bee7
CAO Zheng <caozheng@nuist.edu.cn>
2019-02-02 17:34:30 +0000

    Publish Version 1.0.1

--- BaderMerge.py
+++ BaderMerge.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Version 1.0.1 Created by Z.Cao 2/Feb/2019
 import os, sys, getopt
 import numpy as np
 def cut(line):
@@ -62,7 +63,7 @@ def addlines(linelist, lineindex, needlineindex):
 		flag = startline
 
 		templine = linelist[flag].split()
-		templinecontent = templine[0] + ' '+ templine[1] + '   ' + str(count) + ' ' +templine[3] + '\n'
+		templinecontent = templine[0] + ' '+ templine[1] + ' ' + str(count) + ' ' +templine[3] + '\n'
 		count += 1
 		output += templinecontent
 
